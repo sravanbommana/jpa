16619f740e989a66b7b9f74cc149c60514db3222
Sravan Kumar <bommana@iloguest.org>
2024-06-11 20:08:19 +0530

    Added query parameters params positional change

--- src/main/java/com/persistance/jpa/App.java
+++ src/main/java/com/persistance/jpa/App.java
@@ -17,57 +17,20 @@ public class App
 	//Run 1.4-cascade-entity branch to insert some data before running this 
 	public static void main(String[] args) {
 		App app = new App();
-		//app.getUserByIdWithTypedQuery(1);
-		//app.getUserByIdWithNativeQuery(52);
-		app.getUserByIdWithNamedQuery(52);
+		//app.getUserByIdWithTypedQueryUsingPosition(1);
+		app.getUserByIdWithTypedQueryUsingPositions();
 	}
 	
-	//Example of Named Query
-	public void getUserByIdWithNamedQuery(int id) {
-		EntityManagerFactory factory = Persistence.createEntityManagerFactory("my-persistence-unit");
-		EntityManager manager = factory.createEntityManager();
-		manager.getTransaction().begin();
-		Query query = manager.createNamedQuery("User.findByUserID");
-		query.setParameter("id", id);
-		List<User> user = query.getResultList();
-		user.forEach(ele -> {
-			System.out.println(ele.getName());
-			System.out.println(ele.getDlNumber());
-			List<Vehicle> list = ele.getVehicle();
-			list.forEach(vehicle -> System.out.println(vehicle.getVehicleType()));
-		});
-		manager.getTransaction().commit();
-		manager.close();
-	}
 
 	
-	//Example of Native Query
-	public void getUserByIdWithNativeQuery(int id) {
-		EntityManagerFactory factory = Persistence.createEntityManagerFactory("my-persistence-unit");
-		EntityManager manager = factory.createEntityManager();
-		manager.getTransaction().begin();
-		Query jpqlNativeQuery = manager.createNativeQuery("Select * from User where id=:id", User.class);
-		jpqlNativeQuery.setParameter("id", id);
-		List<User> user = jpqlNativeQuery.getResultList();
-		user.forEach(ele -> {
-			System.out.println(ele.getName());
-			System.out.println(ele.getDlNumber());
-			List<Vehicle> list = ele.getVehicle();
-			list.forEach(vehicle -> System.out.println(vehicle.getVehicleType()));
-		});
-		manager.getTransaction().commit();
-		manager.close();
-
-	}
-	
-	// Example of Typed Query
-	public void getUserByIdWithTypedQuery(int id) {
+	// Example of Typed Query Positional
+	public void getUserByIdWithTypedQueryUsingPosition(int id) {
 		EntityManagerFactory factory = Persistence.createEntityManagerFactory("my-persistence-unit");
 		EntityManager manager = factory.createEntityManager();
 		manager.getTransaction().begin();
 		System.out.println("*****************************************************************");
-		TypedQuery<User> query = manager.createQuery("SELECT u FROM User u where u.id=:id", User.class);
-		query.setParameter("id", id);
+		TypedQuery<User> query = manager.createQuery("SELECT u FROM User u where u.id=?1", User.class);
+		query.setParameter(1, id);
 		List<User> user = query.getResultList();
 		user.forEach(ele -> {
 			System.out.println(ele.getName());
@@ -79,4 +42,27 @@ public class App
 		manager.getTransaction().commit();
 		manager.close();
 	}
+	
+	// Example of Typed Query Positional array
+	public void getUserByIdWithTypedQueryUsingPositions() {
+		EntityManagerFactory factory = Persistence.createEntityManagerFactory("my-persistence-unit");
+		EntityManager manager = factory.createEntityManager();
+		TypedQuery<User> typedQuery = manager.createQuery("SELECT u FROM User u where id IN ?1", User.class);
+		List<String> list = new ArrayList<String>();
+		list.add("1");
+		list.add("52");
+		typedQuery.setParameter(1, list);
+		List<User> user = typedQuery.getResultList();
+		user.forEach(ele -> {
+			System.out.println(ele.getName());
+			System.out.println(ele.getDlNumber());
+			List<Vehicle> list1 = ele.getVehicle();
+			list1.forEach(vehicle -> System.out.println(vehicle.getVehicleType()));
+		});
+		manager.getTransaction().begin();
+		manager.getTransaction().commit();
+		manager.clear();
+	}
+
+	
 }
